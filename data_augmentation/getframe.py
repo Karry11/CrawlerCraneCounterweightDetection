d69ce70cfc256ccb1a089437aff93433f9efb425
ZHULIII <18260036161@163.com>
2024-03-21 10:00:00 +0800

    argparser_mode_getframe
    argparser_mode_getframe

--- data_augmentation/getframe.py
+++ data_augmentation/getframe.py
@@ -1,17 +1,28 @@
-#获取视频帧
 import cv2
 import os
+import argparse
 
-def extract_frames(video_path, output_dir, num_frames=100):
+def extract_frames(video_path, output_dir, num_frames):
     # 创建输出目录
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    count =1
+    origin_output_dir = output_dir
+    while 1:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            break
+        output_dir = origin_output_dir + str(count)
+        count +=1
 
     # 打开视频文件
     video_capture = cv2.VideoCapture(video_path)
-    print("视频是否打卡{}？".format(video_capture.isOpened))
+    if not video_capture.isOpened():
+        print("无法打开视频文件！")
+        return
+    print("视频已打开")
+
     total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    print("总帧数{}？".format(total_frames))
+    print("视频总帧数:", total_frames)
+
     # 确定每个间隔多少帧取一次
     frame_interval = total_frames // num_frames
 
@@ -35,15 +46,14 @@ def extract_frames(video_path, output_dir, num_frames=100):
 
     # 释放资源
     video_capture.release()
-
     print(f"成功保存了 {saved_count} 张帧.")
 
 if __name__ == "__main__":
-    # 输入视频文件路径
-    video_path ="video_data\超起配重3月8日.mp4"
-
-    # 输出帧的保存目录
-    output_dir = "output_frames"
+    parser = argparse.ArgumentParser(description='Extract frames from a video')
+    parser.add_argument('--video_path', type=str, help='视频文件路径')
+    parser.add_argument('--num_frames', type=int, help='要提取的帧数')
+    parser.add_argument('--output_dir', type=str, default="output_frames",help='输出帧的保存目录',required=False)
+    args = parser.parse_args()
 
     # 提取帧
-    extract_frames(video_path, output_dir, num_frames=100)
+    extract_frames(args.video_path, args.output_dir, args.num_frames)
